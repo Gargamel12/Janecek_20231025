63951380b189d46eeda2375e41cb522f5c4b324e
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2023-10-25 13:09:43 +0200

    Funkčnost

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace Janecek_20231025
         {
             InitializeComponent();
         }
+
+        private void Generate_Grid(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(rowTextbox.Text, out int numRows)&&int.TryParse(rowTextbox.Text, out int numColumns)&&int.TryParse(colorTextbox.Text, out int color))
+            {
+                dynamicGrid.Children.Clear();
+                dynamicGrid.RowDefinitions.Clear();
+                dynamicGrid.ColumnDefinitions.Clear();
+                for(int row = 0; row < numRows; row++)
+                {
+                   dynamicGrid.RowDefinitions.Add(new RowDefinition {Height = new GridLength(25) });
+                   for (int col = 0; col < numColumns; col++)
+                   {
+                      if(row==0)
+                      {
+                      dynamicGrid.ColumnDefinitions.Add(new ColumnDefinition {Width = new GridLength(25)});
+                      }
+                        var textBox = new TextBox();
+                        textBox.Text = $"Row{row},Column{col}";
+                        Grid.SetRow(textBox, row);
+                        Grid.SetColumn(textBox, col);
+                        dynamicGrid.Children.Add(textBox);
+                        if((row) == color-1 )
+                        {
+                            textBox.Background= Brushes.Green ;
+                        }
+                        else if(col == color-1)
+                        {
+                            textBox.Background = Brushes.Green;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Zadejte platná čísla pro počet řádků a sloupců");
+            }
+
+        }
     }
 }
